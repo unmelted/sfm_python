56af1663c2e80ad0d0f087ff48ac50b8a120061a
Eunkyung Ma <ekma@4dreplay.com>
2022-04-15 00:47:30 +0900

    0415 midnight

--- adjust.py
+++ adjust.py
@@ -1,8 +1,10 @@
 import os
 import sys
+import numpy as np
 import cv2
 import logging
 from world import *
+from util import *
 
 class Adjust(object):
 
@@ -11,13 +13,16 @@ class Adjust(object):
         self.world = world
         self.world_points = []  # x, y, 4 points
         
-
     def get_first_cp(self):
         pts = [[2749, 785], [884, 1170], [1718, 1541], [3650, 1069]]
         return pts
 
-    def convert_pts(self, cameras):
+    def convert_pts(self, ref, target):
         pass
+    
+    def conver_center(self, ref, target) :
+        pass
+
 
     def write_file(self):
         pass
