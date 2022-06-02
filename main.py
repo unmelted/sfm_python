9dab1d38ceeefedbc9525f0a9a39791b58b4023a
Eunkyung Ma <ekma@4dreplay.com>
2022-06-02 15:39:58 +0900

    cross over FLASK SERVER VER

--- main.py
+++ main.py
@@ -1,65 +1,98 @@
-from cam_group import *
+import os
+import time
+
+from flask import Flask
+from flask import request, jsonify
+from flask_restx import fields, Resource, Api
+
+import json
+
 import numpy as np
 import logging
 import argparse
-import visualize
+
 from extn_util import * 
+from cam_group import *
 
-def run(args):
 
-    logging.basicConfig(level=logging.INFO)
+app = Flask(__name__)
+api = Api(app, version='0.1', title='AUTO CALIB.', description='exodus from slavery')
+app.config.SWAGGER_UI_DOC_EXPANSION = 'full'
 
-    preset1 = Group()
+recon_args = api.model('model' , {
+    'root_path' : fields.String,
+    'mode' : fields.String
+})
 
-    if args.mode == 'colmap' :
-        ret = preset1.create_group_colmap(args.root_dir)        
-    else:
-        ret = preset1.create_group(args.root_dir)
+@api.route('/exodus/run')
+@api.doc()
+class autocalib(Resource) : 
+    @api.expect(recon_args)
+    def run(self, recon_args):
+        parser = api.parser()
+        parser.add_argement('root_path', type=str)
+        parser.adD_argement('mode', type=str)
+        args = parser.parse_args()
 
-    if( ret < 0 ):
-        logging.error("terminated. ")
-        return 0
+        logging.basicConfig(level=logging.INFO)
 
-    if args.mode == 'sfm' : 
-        preset1.run_sfm()
-        preset1.generate_points(args.mode)    
-        preset1.calculate_real_error()
-        preset1.export()
-        preset1.visualize()
+        preset1 = Group()
 
-    elif args.mode == 'vis' :
-        preset1.read_cameras(args.mode)
-        # import_camera_pose(preset1)        
-        preset1.visualize(args.mode)
+        if args['mode'] == 'colmap' :
+            ret = preset1.create_group_colmap(args['root_dir']) 
+        else:
+            ret = preset1.create_group(args['root_dir'])
 
-    elif args.mode == 'eval':
-        preset1.read_cameras(args.mode)
-        # import_camera_pose(preset1)
-        preset1.generate_points(args.mode)            
-        preset1.calculate_real_error()
-        preset1.export()
-        preset1.visualize(args.mode)
+        if( ret < 0 ):
+            logging.error("terminated. ")
+            return 0
 
-    elif args.mode == 'test' :
-        preset1.calculate_real_error()
+        if args['mode'] == 'sfm' : 
+            preset1.run_sfm()
+            preset1.generate_points(args['mode'])    
+            preset1.calculate_real_error()
+            preset1.export()
+            preset1.visualize()
 
-    elif args.mode == 'colmap' :
-        preset1.read_cameras(args.mode)        
-        preset1.generate_points(args.mode)    
-        preset1.calculate_real_error()
-        preset1.export()
-        preset1.visualize(args.mode)
-    
-def set_args(parser):
+        elif args['mode'] == 'vis' :
+            preset1.read_cameras(args['mode'])
+            # import_camera_pose(preset1)        
+            preset1.visualize(args['mode'])
 
-    parser.add_argument('--root_dir', action='store', type=str, dest='root_dir',
-                        help='root directory containing the images/ folder')
-    parser.add_argument('--mode', action='store', type=str, dest='mode', default='sfm',
-                    help='select mode sfm , visualize')
+        elif args['mode'] == 'eval':
+            preset1.read_cameras(args['mode'])
+            # import_camera_pose(preset1)
+            preset1.generate_points(args['mode'])            
+            preset1.calculate_real_error()
+            preset1.export()
+            preset1.visualize(args['mode'])
+
+        elif args['mode'] == 'test' :
+            preset1.calculate_real_error()
+
+        elif args['mode'] == 'colmap' :
+            preset1.read_cameras(args['mode'])        
+            preset1.generate_points(args['mode'])    
+            preset1.calculate_real_error()
+            preset1.export()
+            preset1.visualize(args['mode'])
+        
+    # def set_args(parser):
+
+    #     parser.add_argument('--root_dir', action='store', type=str, dest='root_dir',
+    #                         help='root directory containing the images/ folder')
+    #     parser.add_argument('--mode', action='store', type=str, dest='mode', default='sfm',
+    #                     help='select mode sfm , visualize')
+
+''' file mode main '''
+# if __name__ == '__main__':
+
+#     parser = argparse.ArgumentParser()
+#     set_args(parser)
+#     args = parser.parse_args()
+#     run(args)
+
 
 if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    set_args(parser)
-    args = parser.parse_args()
-    run(args)
+    
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
