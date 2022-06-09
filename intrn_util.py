2efcc6ba7b198598a30d7e7f86134047ef421784
Eunkyung Ma <ekma@4dreplay.com>
2022-06-09 11:37:04 +0900

    result update also when status 100 update

--- intrn_util.py
+++ intrn_util.py
@@ -8,7 +8,9 @@ import definition as df
 def status_update(job_id, status) :
     l.get().w.info("Status update. JOB_ID: {} Status: {} ".format(job_id, status))
     DbManager.getInstance().update('command', status=status, job_id=job_id)
-
+    if status == 100 :
+        finish(job_id, 100)
+        
 def finish(job_id, result) :
     msg = df.get_err_msg(result)
     l.get().w.error("JOB_ID: {} Result: {} Message: {}".format(job_id, result, msg))
