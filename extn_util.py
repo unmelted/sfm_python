eea05966eff72fa3f5706a0e4b160b14c2b3ef07
Kelly(MaEunKyoung) <unmelted@naver.com>
2022-10-20 14:10:51 +0900

    file remove not in group

--- extn_util.py
+++ extn_util.py
@@ -362,6 +362,10 @@ def get_camera_list_in_both(from_path, group_id, ext):
 
         if cam_id in cam_inpts:
             image_names.append(img_file)
+        else:
+            target = from_path + img_file
+            print('this file is not in group. remove.', target)
+            os.remove(target)
 
     if len(image_names) == result:
         l.get().w.info("Image file = dsc_id in pts file same count")
