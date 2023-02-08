0af8824dc663e43f2162c396ebe4ce99630dc668
Kelly(MaEunKyoung) <unmelted@naver.com>
2023-02-08 18:03:56 +0900

    Service start file modify path

--- sh/exodus.sh
+++ sh/exodus.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #local test
-cd /home/replay/work/sfm_python
+cd /home/replay/work/sfm_python/src/
 
 #production
-#cd /home/ubuntu/work/sfm_python
+#cd /home/ubuntu/work/sfm_python/src/
 
 #/home/replay/anaconda3/bin/conda activate calib
 /home/replay/anaconda3/envs/calib/bin/python main.py
